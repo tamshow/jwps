19778f5ade1f479275df8aee1336ff8d3a70acf8
田村章吾 <tamura@MacBook-Pro.local>
2018-11-23 17:08:59 +0900

    modify :clean: XDファイルの調整

--- docs/assets/js/bundle.js
+++ docs/assets/js/bundle.js
@@ -428,6 +428,125 @@ $(function () {
 
 $(function () {
 
+
+  /**
+   * ------------------------------------------------------------------------
+   * Constants
+   * ------------------------------------------------------------------------
+   */
+
+
+  var NAME    = 'modal';
+  var VERSION = '0.5.0';
+
+  var Selector = {
+    BODY        : '[data-modal]',
+    OPEN        : '[data-open-modal]',
+    CLOSE       : '[data-close-modal]',
+    APPEND      : '[data-append-modal]',
+    AREA_HIDDEN : 'header, footer, main',
+    BG          : '.c-modal-dialog-bg'
+  };
+
+  var Default = {
+    current_scrollY : null
+  };
+
+
+
+  if ($(Selector.BODY).length) {
+
+    $('body').append('<div class="c-modal-dialog-bg" data-close-modal aria-expanded="true" aria-label="閉じる"></div>');
+    $(Selector.BG).css({
+      display: 'none',
+      width: '100%',
+      height: '100%',
+      backgroundColor: 'rgba(0, 0, 0, .5)',
+      overflow: 'hidden',
+      position: 'absolute',
+      top: '0',
+      left: '0',
+      zIndex: '99998'
+    });
+
+  }
+
+
+  /**
+   * ------------------------------------------------------------------------
+   * Event
+   * ------------------------------------------------------------------------
+   */
+
+
+    $(Selector.OPEN).on('click touchend', function(e) {
+      show(e);
+    });
+
+    $(document).on('keyup', function(e) {
+      var ESCAPE_KEY_CODE = 27;
+      if (e.keyCode === ESCAPE_KEY_CODE) {
+        hide(e);
+      }
+    });
+
+    $(Selector.CLOSE).on('click touchend', function(e) {
+      hide(e);
+    });
+
+    $(Selector.BG).on('click touchend', function(e) {
+      hide(e);
+    });
+
+  /**
+   * ------------------------------------------------------------------------
+   * Function
+   * ------------------------------------------------------------------------
+   */
+
+
+  function show(e) {
+    e.preventDefault();
+    var $openButton = $(e.currentTarget);
+    var containerAttr = $openButton.attr('aria-controls');
+    var $container = $('#' + containerAttr );
+
+    $(Selector.BG).fadeIn(400);
+    $container.fadeIn(400).attr({'aria-hidden': 'false', 'tabindex': '1'}).focus();
+    $(Selector.AREA_HIDDEN).attr({'aria-hidden': 'true'});
+    Default.current_scrollY = $(window).scrollTop();
+
+    $('body').css({
+      position: 'fixed',
+      width: '100%',
+      top: -1 * Default.current_scrollY
+    });
+
+    var $clone = $('[data-clone-modal="' + containerAttr + '"]');
+
+    if ($clone.length && !$(Selector.APPEND).children().attr('data-clone-modal')) {
+      var elem = $clone.clone(true);
+
+      elem.find('.c-modal-dialog-none').remove();
+      elem.removeAttr('style class').find('*').removeAttr('style class');
+      $(Selector.APPEND).append(elem);
+    }
+  }
+
+
+  function hide() {
+    $(Selector.BG).fadeOut(0);
+    $(Selector.BODY).fadeOut(0).attr({'aria-hidden': 'true', 'tabindex': '-1'});
+    $(Selector.AREA_HIDDEN).removeAttr('aria-hidden');
+    $('body').attr({style: ''});
+    $('html, body').prop({scrollTop: Default.current_scrollY});
+    $(Selector.APPEND).empty();
+  }
+
+
+});
+$(function () {
+
   /**
    * ------------------------------------------------------------------------
    * Constants
@@ -556,125 +675,6 @@ $(function () {
 
 
 
-$(function () {
-
-
-  /**
-   * ------------------------------------------------------------------------
-   * Constants
-   * ------------------------------------------------------------------------
-   */
-
-
-  var NAME    = 'modal';
-  var VERSION = '0.5.0';
-
-  var Selector = {
-    BODY        : '[data-modal]',
-    OPEN        : '[data-open-modal]',
-    CLOSE       : '[data-close-modal]',
-    APPEND      : '[data-append-modal]',
-    AREA_HIDDEN : 'header, footer, main',
-    BG          : '.c-modal-dialog-bg'
-  };
-
-  var Default = {
-    current_scrollY : null
-  };
-
-
-
-  if ($(Selector.BODY).length) {
-
-    $('body').append('<div class="c-modal-dialog-bg" data-close-modal aria-expanded="true" aria-label="閉じる"></div>');
-    $(Selector.BG).css({
-      display: 'none',
-      width: '100%',
-      height: '100%',
-      backgroundColor: 'rgba(0, 0, 0, .5)',
-      overflow: 'hidden',
-      position: 'absolute',
-      top: '0',
-      left: '0',
-      zIndex: '99998'
-    });
-
-  }
-
-
-  /**
-   * ------------------------------------------------------------------------
-   * Event
-   * ------------------------------------------------------------------------
-   */
-
-
-    $(Selector.OPEN).on('click touchend', function(e) {
-      show(e);
-    });
-
-    $(document).on('keyup', function(e) {
-      var ESCAPE_KEY_CODE = 27;
-      if (e.keyCode === ESCAPE_KEY_CODE) {
-        hide(e);
-      }
-    });
-
-    $(Selector.CLOSE).on('click touchend', function(e) {
-      hide(e);
-    });
-
-    $(Selector.BG).on('click touchend', function(e) {
-      hide(e);
-    });
-
-  /**
-   * ------------------------------------------------------------------------
-   * Function
-   * ------------------------------------------------------------------------
-   */
-
-
-  function show(e) {
-    e.preventDefault();
-    var $openButton = $(e.currentTarget);
-    var containerAttr = $openButton.attr('aria-controls');
-    var $container = $('#' + containerAttr );
-
-    $(Selector.BG).fadeIn(400);
-    $container.fadeIn(400).attr({'aria-hidden': 'false', 'tabindex': '1'}).focus();
-    $(Selector.AREA_HIDDEN).attr({'aria-hidden': 'true'});
-    Default.current_scrollY = $(window).scrollTop();
-
-    $('body').css({
-      position: 'fixed',
-      width: '100%',
-      top: -1 * Default.current_scrollY
-    });
-
-    var $clone = $('[data-clone-modal="' + containerAttr + '"]');
-
-    if ($clone.length && !$(Selector.APPEND).children().attr('data-clone-modal')) {
-      var elem = $clone.clone(true);
-
-      elem.find('.c-modal-dialog-none').remove();
-      elem.removeAttr('style class').find('*').removeAttr('style class');
-      $(Selector.APPEND).append(elem);
-    }
-  }
-
-
-  function hide() {
-    $(Selector.BG).fadeOut(0);
-    $(Selector.BODY).fadeOut(0).attr({'aria-hidden': 'true', 'tabindex': '-1'});
-    $(Selector.AREA_HIDDEN).removeAttr('aria-hidden');
-    $('body').attr({style: ''});
-    $('html, body').prop({scrollTop: Default.current_scrollY});
-    $(Selector.APPEND).empty();
-  }
-
-
-});
 $(function () {
 
 
@@ -857,6 +857,88 @@ $(function () {
    * ------------------------------------------------------------------------
    */
 
+  var NAME = 'switch';
+  var VERSION = '0.5.0';
+
+  var Selector = {
+    SWITCH        : '[data-switch]',
+    TARGET        : '[data-toggle-switch]',
+    BODY          : '[data-body-switch]',
+    DEVICE        : '[data-device-switch]'//all, pc, tab, sp
+  };
+
+
+  var Default = {
+    TAB_W  : '960px',
+    SP_W   : '600px'
+  };
+
+
+  $.Event('E_ENTER_KYE_CODE', {keyCode: 13, which: 13});
+
+
+  /**
+   * ------------------------------------------------------------------------
+   * Event
+   * ------------------------------------------------------------------------
+   */
+
+
+  $(Selector.TARGET).on('click touchend E_ENTER_KYE_CODE', function (e) {
+
+    var media = $(e.currentTarget).parents(Selector.DEVICE).data('device-switch') || 'all';
+    var isMobile = window.matchMedia('(max-width:' + Default.SP_W + ')').matches || false;
+    var isTablet = window.matchMedia('(min-width:' + Default.SP_W + ') and (max-width:' + Default.TAB_W + ')').matches || false;
+
+    if (media.match(/all/) ||
+        (media.match(/sp/) && isMobile) ||
+        (media.match(/tab/) && isTablet) ||
+        (media.match(/pc/) && ( !isMobile && !isTablet))
+    ) {
+      toggle(e);
+    }
+  });
+  
+
+  /**
+   * ------------------------------------------------------------------------
+   * Function
+   * ------------------------------------------------------------------------
+   */
+
+  
+  function toggle(e) {
+    e.preventDefault();
+    var $current_target = $(e.currentTarget);
+    var $containerSW = $current_target.parents(Selector.SWITCH);
+    var $bodySW = $containerSW.find(Selector.BODY);
+
+    if ($containerSW.hasClass('is-active')) {
+      $containerSW.removeClass('is-active');
+      $current_target.attr({'aria-expanded': 'true', 'aria-label': '閉じる'});
+      $bodySW.stop().attr('aria-hidden', 'true');
+    } else {
+      $containerSW.addClass('is-active');
+      $current_target.attr({'aria-expanded': 'false', 'aria-label': '開く'});
+      $bodySW.stop().attr('aria-hidden', 'false');
+
+    }
+  }
+
+
+
+
+
+});
+$(function () {
+
+
+  /**
+   * ------------------------------------------------------------------------
+   * Constants
+   * ------------------------------------------------------------------------
+   */
+
   var NAME = 'tab';
   var VERSION = '0.5.0';
 
@@ -954,86 +1036,3 @@ $(function () {
 
 
 
-
-$(function () {
-
-
-  /**
-   * ------------------------------------------------------------------------
-   * Constants
-   * ------------------------------------------------------------------------
-   */
-
-  var NAME = 'switch';
-  var VERSION = '0.5.0';
-
-  var Selector = {
-    SWITCH        : '[data-switch]',
-    TARGET        : '[data-toggle-switch]',
-    BODY          : '[data-body-switch]',
-    DEVICE        : '[data-device-switch]'//all, pc, tab, sp
-  };
-
-
-  var Default = {
-    TAB_W  : '960px',
-    SP_W   : '600px'
-  };
-
-
-  $.Event('E_ENTER_KYE_CODE', {keyCode: 13, which: 13});
-
-
-  /**
-   * ------------------------------------------------------------------------
-   * Event
-   * ------------------------------------------------------------------------
-   */
-
-
-  $(Selector.TARGET).on('click touchend E_ENTER_KYE_CODE', function (e) {
-
-    var media = $(e.currentTarget).parents(Selector.DEVICE).data('device-switch') || 'all';
-    var isMobile = window.matchMedia('(max-width:' + Default.SP_W + ')').matches || false;
-    var isTablet = window.matchMedia('(min-width:' + Default.SP_W + ') and (max-width:' + Default.TAB_W + ')').matches || false;
-
-    if (media.match(/all/) ||
-        (media.match(/sp/) && isMobile) ||
-        (media.match(/tab/) && isTablet) ||
-        (media.match(/pc/) && ( !isMobile && !isTablet))
-    ) {
-      toggle(e);
-    }
-  });
-  
-
-  /**
-   * ------------------------------------------------------------------------
-   * Function
-   * ------------------------------------------------------------------------
-   */
-
-  
-  function toggle(e) {
-    e.preventDefault();
-    var $current_target = $(e.currentTarget);
-    var $containerSW = $current_target.parents(Selector.SWITCH);
-    var $bodySW = $containerSW.find(Selector.BODY);
-
-    if ($containerSW.hasClass('is-active')) {
-      $containerSW.removeClass('is-active');
-      $current_target.attr({'aria-expanded': 'true', 'aria-label': '閉じる'});
-      $bodySW.stop().attr('aria-hidden', 'true');
-    } else {
-      $containerSW.addClass('is-active');
-      $current_target.attr({'aria-expanded': 'false', 'aria-label': '開く'});
-      $bodySW.stop().attr('aria-hidden', 'false');
-
-    }
-  }
-
-
-
-
-
-});
